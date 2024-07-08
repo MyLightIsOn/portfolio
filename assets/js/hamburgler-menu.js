9476f22db93c86f159c5db4ab56ce0d90fbdfc2d
Lawrence Moore <thelawrencemoore@gmail.com>
2024-07-08 12:05:44 -0700

    Finished styling homepage case-study links

--- assets/js/hamburgler-menu.js
+++ assets/js/hamburgler-menu.js
@@ -1,25 +1,28 @@
-(function($) {
-    "use strict";
+(function ($) {
+  "use strict";
 
-    document.getElementById('hamburgler').addEventListener('click', checkNav);
-    window.addEventListener("keyup", function(e) {
-        if (e.keyCode === 27) closeNav();
-    }, false);
+  document.getElementById("hamburgler").addEventListener("click", checkNav);
+  window.addEventListener(
+    "keyup",
+    function (e) {
+      if (e.keyCode === 27) closeNav();
+    },
+    false,
+  );
 
-    function checkNav() {
-        if (document.body.classList.contains('hamburgler-active')) {
-            closeNav();
-        } else {
-            openNav();
-        }
+  function checkNav() {
+    if (document.body.classList.contains("hamburgler-active")) {
+      closeNav();
+    } else {
+      openNav();
     }
+  }
 
-    function closeNav() {
-        document.body.classList.remove('hamburgler-active');
-    }
+  function closeNav() {
+    document.body.classList.remove("hamburgler-active");
+  }
 
-    function openNav() {
-        document.body.classList.add('hamburgler-active');
-    }
-
+  function openNav() {
+    document.body.classList.add("hamburgler-active");
+  }
 })(jQuery);
